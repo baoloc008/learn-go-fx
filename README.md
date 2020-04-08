054c779616a66a2d573532e2a2b4e606dc2bcfcc
baoloc008 <baoloc008@gmail.com>
2020-04-08 23:29:01 +0700

    use lifecycle to start and stop server

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Learn Go Fx
 
+Basic DI is straightforward in Go:
+
+- Higher-layer package calls interface functions, not knowing (nor caring) which package implements them
+- Lower-layer package implements interface functions
+- startup code injects lower-layer implementation via higher-layer constructors
+
 ## Links
 - https://blog.huyage.dev/posts/simple-dependency-injection-in-go/
 - https://ewanvalentine.io/my-thoughts-on-uber-fx/
+
+## Note
+- Fx là dependency injection framework, vì là framework nên cần viết code theo cấu trúc của nó
+- Fx có lifecycle, có thể gọi các hàm lúc app start, stop
--- main.go
+++ main.go
@@ -1,8 +1,51 @@
 package main
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"go.uber.org/fx"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	app := fx.New()
+	app := fx.New(fx.Invoke(startServer))
 	app.Run()
+}
+
+var listenAddr = ":9620"
+
+func startServer(lifecycle fx.Lifecycle) {
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	srv := http.Server{
+		Addr:    listenAddr,
+		Handler: router,
+	}
+
+	lifecycle.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+					log.Fatalf("Server ListenAndServe Error: %v", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			log.Println("Server is shutting down...")
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			err := srv.Shutdown(ctx)
+			if err != nil {
+				log.Fatalf("Server Shutdown Error: %v", err)
+			} else {
+				log.Println("Server Shutdown Properly")
+			}
+			return err
+		},
+	})
 }
