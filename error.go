a977c53925e4e770305813531c562bd2d9a9e3d5
baoloc008 <baoloc008@gmail.com>
2020-04-09 11:39:42 +0700

    add example code

---
+++ error.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"go.uber.org/fx"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// A module that provides a HTTP server depends on
+	// the $PORT environment variable. If the variable
+	// is unset, the module returns an fx.Error option.
+	newHTTPServer := func() fx.Option {
+		port := os.Getenv("PORT")
+		if port == "" {
+			return fx.Error(errors.New("$PORT is not set"))
+		}
+		return fx.Provide(&http.Server{
+			Addr: fmt.Sprintf(":%s", port),
+		})
+	}
+
+	app := fx.New(
+		newHTTPServer(),
+		fx.Invoke(func(s *http.Server) error { return s.ListenAndServe() }),
+	)
+
+	fmt.Println(app.Err())
+}
